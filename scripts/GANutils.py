0e210ebb6217d031bee4e88bad949e92b66d2d03
peterzhefu <peterzhefu@gmail.com>
2019-02-22 00:20:49 +0000

    corrected syntax error to move from python2 to python3

--- scripts/GANutils.py
+++ scripts/GANutils.py
@@ -50,7 +50,7 @@ def make_inputs_and_surfaces(file_batch, voxel_dir):
 
 
 def save_voxels(save_dir, models, epock, recon_models = None): 
-    print "Saving the model"
+    print("Saving the model")
     np.save(save_dir+str(epock)  , models) 
     if recon_models is not None: 
         np.save(save_dir+str(epock) + '_VAE', recon_models) 
